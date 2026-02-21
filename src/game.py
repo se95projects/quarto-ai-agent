9de4e45979f06a9118d88bf0352a2f2e5bf55ea6
Saura Elghonimi <selghonimi@gmail.com>
2026-02-21 15:01:27 -0600

    finished make move and unit tests

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
-from src.models import GameState, Piece
+from src.models import GameState, Piece, GamePhase
+from typing import Optional
 
 def get_legal_placements(state: GameState) -> list[tuple[int, int]]:
     return [
@@ -7,3 +8,48 @@ def get_legal_placements(state: GameState) -> list[tuple[int, int]]:
 
 def get_legal_piece_selections(state: GameState) -> list[Piece]:
     return state.remaining_pieces
+
+def make_move(
+    state: GameState,
+    placement: Optional[tuple[int, int]] = None,
+    piece_to_give: Optional[Piece] = None
+) -> GameState:
+
+    if state.current_phase == GamePhase.SELECT_PIECE:
+        if piece_to_give is None:
+            raise ValueError("Must provide a piece to give during SELECT_PIECE phase.")
+
+        legal_pieces = get_legal_piece_selections(state)
+
+        if piece_to_give in legal_pieces:
+            state.selected_piece = piece_to_give
+            state.remaining_pieces.remove(piece_to_give)
+
+            if state.current_player == 0:
+                state.current_player = 1 
+            else:
+                state.current_player = 0
+
+            state.current_phase = GamePhase.PLACE_PIECE
+        else:
+            raise ValueError("Piece is not available for selection.")
+
+    elif state.current_phase == GamePhase.PLACE_PIECE:
+        if placement is None:
+            raise ValueError("Must provide a placement during PLACE_PIECE phase.")
+
+        legal_placements = get_legal_placements(state)
+
+        if placement in legal_placements:
+            state.board.place(
+                piece = state.selected_piece, 
+                row = placement[0],
+                col = placement[1]
+            )
+            state.selected_piece = None
+
+            state.current_phase = GamePhase.SELECT_PIECE
+        else:
+            raise ValueError("Position is already occupied or out of bounds.")
+
+    return state
--- tests/test_game.py
+++ tests/test_game.py
@@ -4,7 +4,8 @@ import pytest
 from src.models import Piece, Board, GamePhase, GameState
 from src.game import (
     get_legal_placements,
-    get_legal_piece_selections
+    get_legal_piece_selections,
+    make_move
 )
 
 class TestGetLegalPlacement:
@@ -112,3 +113,113 @@ class TestLegalPieceSelection:
         result = get_legal_piece_selections(state)
         assert piece not in result
         assert len(result) == 15
+
+class TestMakeMove:
+    def test_make_move_piece_not_given_during_select(self):
+        state = GameState(
+            board=Board.empty(),
+            remaining_pieces=[],
+            current_phase=GamePhase.SELECT_PIECE,
+            selected_piece=None,
+            current_player=0
+        )
+
+        with pytest.raises(ValueError, match="Must provide a piece to give during SELECT_PIECE phase."):
+            make_move(state)
+
+    def test_make_move_placement_not_given_during_place(self):
+        state = GameState(
+            board=Board.empty(),
+            remaining_pieces=[],
+            current_phase=GamePhase.PLACE_PIECE,
+            selected_piece=None,
+            current_player=0
+        )
+
+        with pytest.raises(ValueError, match="Must provide a placement during PLACE_PIECE phase."):
+            make_move(state)
+
+    def test_make_move_invalid_placement(self):
+        piece = Piece(height=True, color=True, shape=True, top=True)
+        placement = (0, 0)
+        board = Board.empty()
+        board.place(piece=piece, row=placement[0], col=placement[1])
+
+        state = GameState(
+            board=board,
+            remaining_pieces=[],
+            current_phase=GamePhase.PLACE_PIECE,
+            selected_piece=None,
+            current_player=0
+        )
+
+        with pytest.raises(ValueError, match="Position is already occupied or out of bounds."):
+            make_move(state=state, placement=placement)
+
+    def test_make_move_invalid_piece(self):
+        remaining_pieces = [
+            Piece(height=h, color=c, shape=s, top=t)
+            for h, c, s, t in product([True, False], repeat=4)
+        ]
+        piece = Piece(height=True, color=True, shape=True, top=True)
+        remaining_pieces.remove(piece)
+
+        board = Board.empty()
+        board.place(piece=piece, row=0, col=0)
+
+        state = GameState(
+            board=board,
+            remaining_pieces=remaining_pieces,
+            current_phase=GamePhase.SELECT_PIECE,
+            selected_piece=piece,
+            current_player=0
+        )
+
+        with pytest.raises(ValueError, match="Piece is not available for selection."):
+            make_move(state=state, piece_to_give=piece)
+
+    def test_make_move_valid_placement(self):
+        remaining_pieces = [
+            Piece(height=h, color=c, shape=s, top=t)
+            for h, c, s, t in product([True, False], repeat=4)
+        ]
+
+        placement = (0,0)
+        piece = Piece(height=True, color=True, shape=True, top=True)
+        current_player = 0
+
+        state = GameState(
+            board=Board.empty(),
+            remaining_pieces=remaining_pieces,
+            current_phase=GamePhase.PLACE_PIECE,
+            selected_piece=piece,
+            current_player=current_player
+        )
+
+        result = make_move(state=state, placement=placement)
+        assert result.board.grid[placement[0]][placement[1]] == piece
+        assert result.current_phase == GamePhase.SELECT_PIECE
+        assert result.selected_piece == None
+        assert current_player == result.current_player
+
+    def test_make_move_valid_piece(self):
+        remaining_pieces = [
+            Piece(height=h, color=c, shape=s, top=t)
+            for h, c, s, t in product([True, False], repeat=4)
+        ]
+        piece = Piece(height=True, color=True, shape=True, top=True)
+        current_player = 0
+
+        state = GameState(
+            board=Board.empty(),
+            remaining_pieces=remaining_pieces,
+            current_phase=GamePhase.SELECT_PIECE,
+            selected_piece=None,
+            current_player=current_player
+        )
+
+        result = make_move(state=state, piece_to_give=piece)
+        assert result.selected_piece == piece
+        assert piece not in result.remaining_pieces
+        assert result.current_phase == GamePhase.PLACE_PIECE
+        assert current_player != result.current_player
